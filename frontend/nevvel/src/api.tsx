21d0fc7011642473aa148c8b3cdfdaf428c3ecd9
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-08-22 15:58:01 +0900

    fix : add withCredentials

--- frontend/nevvel/src/api.tsx
+++ frontend/nevvel/src/api.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 // }
 
 const springApi = axios.create({
+  withCredentials: true,
   baseURL: "https://www.nevvel.net:8081/api",
   // baseURL: "http://3.35.53.215:8080//api"
   // baseURL: "https://k8d106.p.ssafy.io:8080/api"
@@ -23,32 +24,32 @@ export const NewvelApi = {
   // users
 
   // 소셜 로그인
-  logIn: () => springApi.get("/oauth2/authorization/kakao", { withCredentials: true }),
+  logIn: () => springApi.get("/oauth2/authorization/kakao"),
   // 로그인 api 이게 맞나요...??
 
   // 에피소드 판매지수 높은 순 인기작가
   bestWriters: () => springApi.get("/users/best"),
 
   // 내 프로필 정보 반환
-  profileInfo: () => springApi.get("/users", { withCredentials: true }),
+  profileInfo: () => springApi.get("/users"),
 
   // covers
 
   // 좋아요한 소설 표지 불러오기
-  likesCovers: () => springApi.get("/covers/likes", { withCredentials: true }),
+  likesCovers: () => springApi.get("/covers/likes"),
 
   // 구매한 소설 표지
-  purchasedCovers: () => springApi.get("/covers/purchased-on", { withCredentials: true }),
+  purchasedCovers: () => springApi.get("/covers/purchased-on"),
 
   // episodes
 
   // 에피소드 구매목록 조회
-  purchasedEpisodes: () => springApi.get("/episodes/purchased-on", { withCredentials: true }),
+  purchasedEpisodes: () => springApi.get("/episodes/purchased-on"),
 
   // assets
 
   // 내가 가지고 있는 에셋 조회
-  purchasedAssets: () => springApi.get("/assets/purchased-on", { withCredentials: true }),
+  purchasedAssets: () => springApi.get("/assets/purchased-on"),
 
   // genre
 
