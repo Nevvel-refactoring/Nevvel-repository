67825153c6a0dd5c71b8697f45bfb89977407318
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-08-23 16:01:32 +0900

    test : 마이페이지 api 테스트

--- frontend/nevvel/src/api/instance.ts
+++ frontend/nevvel/src/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // api request가 있는 경우 | api에 params가 들어가는 경우 -> springApi 호출로 baseURL만 받아서 request 넣거나, api 완성해서 axios 요청
 const springApi = axios.create({
