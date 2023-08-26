8eb22d050cff1945fa08f7a0c528bc8cc0267570
Hokyun <h0kyungim@gmail.com>
2023-08-26 21:30:28 +0900

    test: withCredentials 만으로 해결되는지 확인

--- frontend/nevvel/src/components/layout/MyPageModal.tsx
+++ frontend/nevvel/src/components/layout/MyPageModal.tsx
@@ -34,7 +34,7 @@ export const MyPageModal = ({
     setModal(false);
     onClickProfile();
     axios
-      .post("https://www.nevvel.net:8081/api/users/signout", "로그아웃" ,{headers: {'Content-type': 'text/plain'}, withCredentials: true})
+      .post("https://www.nevvel.net:8081/api/users/signout", "로그아웃" ,{withCredentials: true})
       .then(() => logoutRoute())
       .catch((error) => console.log(error));
   };
