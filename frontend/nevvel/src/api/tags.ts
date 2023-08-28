957ba571b000f9a5f5fc3a977976b6754230e24f
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-08-28 16:29:34 +0900

    test: tag 받아오기 test

--- frontend/nevvel/src/api/tags.ts
+++ frontend/nevvel/src/api/tags.ts
@@ -2,9 +2,10 @@ import springApi from "./instance";
 
 export const getTagList = async () => {
   try {
-    const { data } = await springApi.get("/tags");
+    const data = await springApi.get("/tags");
     return data;
   } catch (error) {
     console.log(error);
+    return null
   }
 };
