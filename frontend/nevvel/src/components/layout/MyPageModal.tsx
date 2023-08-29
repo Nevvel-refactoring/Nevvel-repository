c8ddf5b9e2827cbbe8b1c5d6a538daaa1892098f
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-08-29 16:32:02 +0900

    test: 로그아웃 정상작동 테스트

--- frontend/nevvel/src/components/layout/MyPageModal.tsx
+++ frontend/nevvel/src/components/layout/MyPageModal.tsx
@@ -37,16 +37,17 @@ export const MyPageModal = ({
     const KaKaoLogout = async () => {
       const res = await postLogout();
       if (res === true) {
-        console.log(222);
+        console.log(555);
         // localStorage 초기화
         localStorage.removeItem("loginStatus");
         localStorage.removeItem("userInfoStatus");
         // 로그아웃 후 로그아웃페이지 리다이렉트
-        router.push({ pathname: "/logout" });
+        await router.push({ pathname: "/logout" });
       }
-      console.log(111);
+      console.log(444);
     };
     KaKaoLogout();
+    console.log(666);
     // axios
     //   .post("https://www.nevvel.net:8081/api/users/signout", null, {
     //     withCredentials: true,
