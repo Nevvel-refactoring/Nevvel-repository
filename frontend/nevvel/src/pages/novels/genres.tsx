02d6e698f0ed23dd0013e33279506b25be2695db
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-09-04 15:32:29 +0900

    fix: 메인페이지 소설 항목 에러 수정

--- frontend/nevvel/src/pages/novels/genres.tsx
+++ frontend/nevvel/src/pages/novels/genres.tsx
@@ -188,7 +188,7 @@ export async function getServerSideProps(context: {
     pageNum = Number(context.query.pageNum);
   }
   try {
-    const res = await axios.get(process.env.NEXT_PUBLIC_API_BASE_URL!, {
+    const res = await axios.get("https://www.nevvel.net:8081/api/covers", {
       params: {
         sorttype: sort,
         page: pageNum,
