5fa4274200181ebda315cc93d9c7d3eb9ea77d75
kimhhyeonjin <kimhhyeonjin@gmail.com>
2023-09-04 17:14:52 +0900

    fix: 이미지 url 수정

--- frontend/nevvel/next.config.js
+++ frontend/nevvel/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["nevvel.s3.ap-northeast-2.amazonaws.com", "https://www.nevvel.net:8081"],
+    domains: ["nevvel.s3.ap-northeast-2.amazonaws.com", "https://www.nevvel.net"],
   },
   webpack: (config) => {
     config.module.rules.push({
